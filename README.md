fc1128cbf7da77535f6187262db4ff4684a0e19b
Carl Bergquist <carlbergquist81@gmail.com>
2022-07-22 04:33:20 -0500

    Final

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Password-Gen
+
+Password Generator
+
+## Contributors
+
+Carl Bergquist
+
+## Project Desciption
+
+With this project we were given code to work with so that we could make it operational. So that once the 'generate password' button is clicked it will function as intended.
+
+## Usage
+
+Navigate to my deployed repository, then click the button and follow the prompts. Once completed you will be given your randomly generated password.
+
+## Image of finished product
+
+![Final screenshot of completed assignment](./Final.png)
+
+
+## Link to Application
+
+[Carl Bergquist-Password Generator](https://carlbergquist.github.io/code-refactor/)
--- script.js
+++ script.js
@@ -11,36 +11,71 @@ function writePassword() {
 
 
 
-//generate password
 function generatePassword() {
   var charPick = Number(prompt("How many characters would you like? (must be between: 8-128)"));
-  var charType = confirm("Press OK for Lowercase");
-  var charType1 = confirm("Press OK for for UpperCase");
-  var charType2 = confirm("Press OK for Numbers");
-  var charType3 = confirm("Press OK for Symbols");
-  var charSet = "";
-  if (charType === true) {
-    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
-    console.log(charSet)
-  } else if (charType1 && charType2 && charType3 === true) {
-    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
-  } else if (charType2 && charType3 === true) {
-    charSet = "0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  } else if (charType3 === true) {
-    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  if (charPick < 8 || charPick > 128) {
+    alert("must be between: 8-128");
+    return;
   }
- 
+
+  var charLower = confirm("Press OK for Lowercase");
+  var charUpper = confirm("Press OK for for UpperCase");
+  var charNumbers = confirm("Press OK for Numbers");
+  var charSymbols = confirm("Press OK for Symbols");
+  
+  var randomGen = "";
+
+  //returns all if all selected OK
+  if (charLower && charUpper && charNumbers && charSymbols) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+    
+    // if 3 selected Ok
+  } else if (charLower && charUpper && charNumbers) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  } else if (charLower && charUpper && charSymbols) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+  } else if (charSymbols && charUpper && charNumbers) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+  } else if (charLower && charSymbols && charNumbers) {
+    randomGen = "abcdefghijklmnopqrstuvwxyz!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+    
+    // if 2 selected
+  } else if (charLower && charUpper) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  } else if (charLower && charNumbers) {
+    randomGen = "abcdefghijklmnopqrstuvwxyz0123456789";
+  } else if (charLower && charSymbols) {
+    randomGen = "abcdefghijklmnopqrstuvwxyz!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if (charNumbers && charUpper) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if (charUpper && charSymbols) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if (charSymbols && charNumbers) {
+    randomGen = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+
+    // if 1 selected
+  } else if (charLower && !charUpper && !charNumbers && !charSymbols) {
+    randomGen = "abcdefghijklmnopqrstuvwxyz";
+  } else if (!charLower && charUpper && !charNumbers && !charSymbols) {
+    randomGen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if (!charLower && !charUpper && charNumbers && !charSymbols) {
+    randomGen = "0123456789";
+  } else if (!charLower && !charUpper && !charNumbers && charSymbols) {
+    randomGen = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  
+  //displays alert to select a character if none are selected
+  } else if (!charLower && !charUpper && !charNumbers && !charSymbols) {
+    alert("must select atleast one character type!");
+    return;
+  }
+
+
   var answer = "";
   for (var i = 0; i < charPick; i++) {
-    answer = charSet.charAt(Math.floor(Math.random() * charSet.length));
+    answer += randomGen.charAt(Math.floor(Math.random() * randomGen.length));
   }
   return answer;
 }
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
